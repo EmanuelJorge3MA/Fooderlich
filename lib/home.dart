c2dacfe16cb4353a2b25fdaa5914337fcf06a3f0
emanueljorge <emanuel.jorge.130@gmail.com>
2022-01-04 23:53:05 +0100

    show pages

--- lib/home.dart
+++ lib/home.dart
@@ -9,7 +9,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomState extends State<Home> {
-  // TODO: Add state variables and fuctions
+  // Add state variables and fuctions
+  // 7
+  int _selectedIndex = 0;
+
+  // 8
+  static List<Widget> pages = <Widget>[
+    // TODO: Replace with Card1
+    Container(color: Colors.red),
+    // TODO: Replace with Card2
+    Container(color: Colors.green),
+    // TODO: Replace with Card3
+    Container(color: Colors.blue)
+  ];
+
+  // 9
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +40,33 @@ class _HomState extends State<Home> {
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
-      // TODO: Show selected tab
-      body: Center(
-        child: Text('Let\'s get cooking 👩‍🍳',
-            // 3
-            style: Theme.of(context).textTheme.headline1),
+      // Show selected tab
+      body: pages[_selectedIndex],
+      // Add bottom navigation bar
+      // 4
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        // Set selected tab bar
+        // 10
+        currentIndex: _selectedIndex,
+        // 11
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
       ),
-      // TODO: Add bottom navigation bar
     );
   }
 }
