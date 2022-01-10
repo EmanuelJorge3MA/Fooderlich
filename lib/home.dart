e0fa647dc13376adc0a0999749ed46aa828afd44
Emanuel Jorge <emanuel.jorge.130@gmail.com>
2022-01-10 12:45:36 +0100

    first card

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'card1.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -15,8 +16,9 @@ class _HomState extends State<Home> {
 
   // 8
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with Card1
-    Container(color: Colors.red),
+    // Replace with Card1
+    // Container(color: Colors.red),
+    const Card1(),
     // TODO: Replace with Card2
     Container(color: Colors.green),
     // TODO: Replace with Card3
